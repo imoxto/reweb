ea9d908764d878f428ae4dddd74802c1b1225f44
imoxto <mohammad.rafid.hamid@g.bracu.ac.bd>
2024-12-11 00:02:15 +0600

    basic form

---
+++ src/app/dashboard/organisation/create/page.tsx
@@ -0,0 +1,9 @@
+import { OrganisationCreateForm } from "../_components/organisation-forms";
+
+export default function OrganizationCreatePage() {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <OrganisationCreateForm />
+    </div>
+  );
+}
--- src/app/dashboard/user/account/page.tsx
+++ src/app/dashboard/user/account/page.tsx
@@ -1,7 +1,9 @@
 import { ProfileForm } from "./profile-form";
 
 export default function AccountPage() {
-  return <div className="flex flex-col gap-4 items-center justify-center">
-    <ProfileForm />
-  </div>;
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <ProfileForm />
+    </div>
+  );
 }
